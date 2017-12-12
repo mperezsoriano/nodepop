cd0fb48f60b6d5c34bbedaf5d10c9ffb178cf064
Manuel I. Perez Soriano <mperezsoriano@gmail.com>
2017-12-12 12:20:01 +0100

    Modificacion por constantes y adecuacion del fichero app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express')
+  ,path = require('path')
+  ,favicon = require('serve-favicon')
+  ,logger = require('morgan')
+  ,cookieParser = require('cookie-parser')
+  ,bodyParser = require('body-parser')
 
 var app = express();
 
@@ -22,8 +19,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
