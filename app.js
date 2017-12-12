3ebbd016ee9f78c551e76675d3a2e340168f0298
Manuel I. Perez Soriano <mperezsoriano@gmail.com>
2017-12-12 12:26:28 +0100

    Creada la carpeta apiv1 y el fichero de anuncios.js asi como su routes

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
+app.use('/apiv1/anuncios', require('./routes/apiv1/anuncios'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
