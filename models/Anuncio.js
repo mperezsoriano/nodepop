f9754d090f722f276e3975a0d6bc8436f429689d
Manuel I. Perez Soriano <mperezsoriano@gmail.com>
2017-12-13 02:04:03 +0100

    Implementados los filtros de busqueda a excepcion de los limites y el nombre

--- models/Anuncio.js
+++ models/Anuncio.js
@@ -2,6 +2,20 @@
 
 const mongoose = require('mongoose')
 
+function comparar ( a, b ){ return a - b; }
+
+function breakPrice(precio) {
+  var prices = [0, 0];
+  const mark = precio.search("-")
+  const priceMin = precio.substring(0, mark);
+  const priceMax = precio.substring(mark+1, precio.length);
+  if (priceMin) { prices[0] = priceMin };
+  if (priceMax) { prices[1] = priceMax };
+  if (mark == -1) { prices[0] = priceMax };
+  if (mark == (precio.length)-1) { prices[1] = 100000 };
+  return prices.sort(comparar)
+}
+
 const anuncioSchema = mongoose.Schema({
   nombre: String,
   venta: {type: Boolean, index: true},
@@ -12,6 +26,21 @@ const anuncioSchema = mongoose.Schema({
 },{
   collection: 'anuncios'
 });
+
+anuncioSchema.statics.list = function(filter) {
+  const query = Anuncio.find({})
+  if (filter.tag) { query.where('tags', filter.tag) };
+  if (filter.venta) { query.where('venta', filter.venta) };
+  if (filter.precio) {
+    const precios = breakPrice (filter.precio);
+    query.where('precio').gte(precios[0]);
+    query.where('precio').lte(precios[1]);
+  }
+  if (filter.organize) { query.sort(filter.organize) }
+  query.skip(1).limit(3)
+
+  return query.exec();
+}
 
 const Anuncio = mongoose.model('Anuncio', anuncioSchema);
 
--- routes/apiv1/anuncios.js
+++ routes/apiv1/anuncios.js
@@ -7,31 +7,30 @@ const express = require('express')
   , router = express.Router()
   , joi = require('joi')
   , schema = require('../../lib/validate-anuncios')
-  , composition = require('../../lib/find-anuncios');
+  , composition = require('../../lib/find-anuncios')
+  , Anuncio = require('../../models/Anuncio');
 
 /**
  * mildware para la validacion de los datos de entrada de busqueda de los
  * anuncios.
  */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   const datosQuery = req.query
-  joi.validate(datosQuery, schema, (err, value) => {
-    if (err) { throw(err) }
-    else {
-      //pasamos el req para que nos devuelva una cadena de busqueda em req.find
-      composition(req)
-      next();
-    }
-  })
+  try{
+    const value = await joi.validate(datosQuery, schema);
+    next ();
+  } catch (err) {
+    next (err);
+  }
 });
 
 router.get('/', async (req, res, next) => {
   try {
-    console.log (req.query)
-    const row = await Anuncio.find({}).exec()
-    res.json({succes: true, reult: row})
+    const filters = req.query;
+    const row = await Anuncio.list(filters);
+    res.json({succes: true, reult: row});
   } catch (err) {
-    next (err)
+    next (err);
   }
 });
 
